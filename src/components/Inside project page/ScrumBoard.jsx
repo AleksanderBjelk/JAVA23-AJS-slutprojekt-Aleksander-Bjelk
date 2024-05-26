63fd1709939c4b69b3bbd56c034282b1779db082
AleksanderBjelk <bjelkaleksander@gmail.com>
2024-05-26 13:27:08 +0200

    small import fix

--- src/components/Inside project page/ScrumBoard.jsx
+++ src/components/Inside project page/ScrumBoard.jsx
@@ -1,3 +1,6 @@
+import { ref, set} from "firebase/database";
+import { db } from "../../modules/firebaseConfig";
+
 function ScrumBoard({ assignments, updateAssignmentStatus, removeAssignment }) {
     return (
         <div className="scrum-board">
