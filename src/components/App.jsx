56b91b262a766c77b7258aa1ed57af8bf4048a49
AleksanderBjelk <bjelkaleksander@gmail.com>
2024-05-27 12:19:58 +0200

    test för gthub pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,18 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import NotFound from "./NotFound";
 import Home from "./Start project page/Home";
 import ProjectDetail from "./Inside project page/ProjectDetails";
 
 export function App() {
     return (
-        <BrowserRouter>
-        
-        <Routes>
+        <HashRouter>
 
-           <Route path="/" element={<Home/>}/>
-           <Route path="/project/:projectId" element={<ProjectDetail/>}/>
-           <Route path="*" element={<NotFound/>}/>
-
-
-        </Routes>
-  
-        </BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/project/:projectId" element={<ProjectDetail />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+            
+        </HashRouter>
     );
 }
